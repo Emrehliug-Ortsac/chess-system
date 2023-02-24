e57476391e41ee2e7e52931725dc027f0480c84e
Gui <paulogui702@gmail.com>
2023-02-24 12:31:35 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,7 +24,6 @@ public class ChessMatch {
 		return mat;
 	}
 	
-
 	private void initialSetup() {
 		board.placePiece(new Rook(board, ChessColor.WHITE), new Position(2, 1));
 		board.placePiece(new King(board, ChessColor.BLACK), new Position(0, 4));
