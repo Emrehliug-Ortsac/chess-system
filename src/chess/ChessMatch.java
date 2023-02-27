7e28f7bafd719ed4ced23ff6cf5212c32d947b3f
Gui <paulogui702@gmail.com>
2023-02-27 11:24:40 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,9 +28,19 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board, ChessColor.WHITE));
-		placeNewPiece('e', 8, new King(board, ChessColor.BLACK));
-		placeNewPiece('e', 1, new King(board, ChessColor.BLACK));
+		placeNewPiece('c', 1, new Rook(board, ChessColor.WHITE));
+        placeNewPiece('c', 2, new Rook(board, ChessColor.WHITE));
+        placeNewPiece('d', 2, new Rook(board, ChessColor.WHITE));
+        placeNewPiece('e', 2, new Rook(board, ChessColor.WHITE));
+        placeNewPiece('e', 1, new Rook(board, ChessColor.WHITE));
+        placeNewPiece('d', 1, new King(board, ChessColor.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, ChessColor.BLACK));
+        placeNewPiece('c', 8, new Rook(board, ChessColor.BLACK));
+        placeNewPiece('d', 7, new Rook(board, ChessColor.BLACK));
+        placeNewPiece('e', 7, new Rook(board, ChessColor.BLACK));
+        placeNewPiece('e', 8, new Rook(board, ChessColor.BLACK));
+        placeNewPiece('d', 8, new King(board, ChessColor.BLACK));
 	}
 	
 }
